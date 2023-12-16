b7f1b3ff70c4415aaf3cb94b8e8d2ec1e077d208
boody <bodymagdy97@gmail.com>
2023-12-16 23:08:57 +0200

    x

---
+++ main.js
@@ -0,0 +1,24 @@
+let icon = document.querySelector(".icon");
+let nav = document.querySelector("nav");
+
+function switching(){
+    console.log(nav);
+    nav.classList.toggle("boom");
+
+}
+icon.addEventListener("click",switching);
+
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
