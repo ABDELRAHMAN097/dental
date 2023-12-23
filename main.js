170751b8441700a4fbc8a877557adad32b5201d5
boody <bodymagdy97@gmail.com>
2023-12-23 10:49:29 +0200

    x

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ function switching(){
 
 }
 icon.addEventListener("click",switching);
+// ######
 
-
-
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+  },
+  autoplay: {
+      delay: 3000, // Autoplay delay in milliseconds
+      disableOnInteraction: false, // Continue autoplay after user interactions
+  },
+});
 
 
 
