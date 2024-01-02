c10057ccf1e322148fc092d02b0a2bef019384ca
boody <bodymagdy97@gmail.com>
2024-01-02 09:49:38 +0200

    x

--- main.js
+++ main.js
@@ -9,48 +9,4 @@ function switching(){
 icon.addEventListener("click",switching);
 // ######
 
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-  },
-  autoplay: {
-      delay: 3000, // Autoplay delay in milliseconds
-      disableOnInteraction: false, // Continue autoplay after user interactions
-  },
-});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
